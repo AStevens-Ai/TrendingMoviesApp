c2e3ea73d2f38115c3e772f4780be03ceb450ea9
Alexander Stevens <murakumo@Alexanders-MacBook-Pro.local>
2023-10-25 09:38:39 -0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'e06919fcd74974ea6b1938bc2d324d81',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p'
 };
 
 /*
